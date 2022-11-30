3b3b2113281f1cf739c2504eb55affce65df8fec
Jorge Cruz <jorrge.cruz@gmail.com>
2022-11-30 11:55:09 -0600

    Practica 2, histograma

---
+++ codigo.js
@@ -0,0 +1,11 @@
+let contador;
+let alternada = 1;
+let limite = prompt("Ingresa el numero de terminos de la suma")
+let PI=0;
+
+for (contador=1; contador < limite; contador += 2){
+    PI += alternada*(1/contador);
+    alternada*=(-1);
+}
+PI*=4;
+console.log(PI);
---
+++ histograma.js
@@ -0,0 +1,25 @@
+let datoIngresado;
+let contadorPositivos=0;
+let contadorNegativos=0;
+
+do{
+    datoIngresado = prompt("Ingresa un valor entereo positivo o negativo, termina con 0");
+    console.log(datoIngresado);
+    if(datoIngresado > 0){
+        contadorPositivos++;
+    }else if(datoIngresado<0){
+        contadorNegativos++;
+    }
+}while(datoIngresado !=0)
+let renglon="Positivos:";
+for(let i=0;i<contadorPositivos;i++){
+    renglon+="*";
+}
+console.log(renglon);
+
+renglon="Negativos:";
+for(let i=0;i<contadorNegativos;i++){
+    renglon+="*";
+}
+console.log(renglon);
+
